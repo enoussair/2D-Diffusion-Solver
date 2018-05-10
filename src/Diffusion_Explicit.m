fe7aa79a9430bd17428da6212233e319655966ba
Noussair Erassifi <n.erassifi@gmail.com>
2018-05-09 21:42:19 -0500

    Final Explicit Method

--- src/Diffusion_Explicit.m
+++ src/Diffusion_Explicit.m
@@ -2,7 +2,7 @@ clc
 clear all
 %Splits up the domain of interest. 2 pi is the length
 %and width of the rectangle
-N = 1000;
+N = 40;
 d_x = 2*pi/(N-1);
 d_y = 2*pi/(N-1);
 
@@ -16,7 +16,7 @@ y = a_y:d_y:b_y;
 
 %Time domain
 t = 4; %seconds
-d_t = d_x^2/(8*N-1);
+d_t = d_x^2/4;
 
 %The dirchelet boundary conditions
 f_a = (x-a_x).^2.*cos(pi*x/(a_x)); %top boundary 
@@ -30,22 +30,32 @@ U_init(:,N) = g_a(N) + (y-a_y)/(b_y - a_y)*(f_a(N) - g_a(N));
 
 %Explicit Method
 U_solut = U_init;
-lam = d_t/d_x^2;
+lam = .5*d_t/d_x^2;
 
-for i = 1:d_t:4
+%initial error value, set high to make sure condition is not met automatically
+error = 10;
+
+for t = 0:d_t:10
     U_init = U_solut;
 for i = 2:N-1
     for j = 1:N-1
-        if i == 2 && j > 1
-             U_solut(i,j) = lam*(U_init(i,j-1) + U_init(i, j+1) + U_init(i+1,j) + U_init(i-1,j)) + (1-4*lam)*(U_init(i,j)); 
-        elseif j == 1 && i > 2
-             U_solut(i,j) = lam*(2*U_init(i, j+1) + U_init(i+1,j) + U_init(i-1,j)) + (1-4*lam)*(U_init(i,j)); 
-        elseif i == N-1 && j > 1
-             U_solut(i,j) = lam*(U_init(i,j-1) + U_init(i, j+1) + U_init(i+1,j) + U_init(i-1,j)) + (1-4*lam)*(U_init(i,j));  
-        elseif i > 2 && j == N-1
-             U_solut(i,j) = lam*(U_init(i,j-1) + U_init(i, j+1) + U_init(i+1,j) + U_init(i-1,j)) + (1-4*lam)*(U_init(i,j));       
+        %Neumannn conditions with ghost node
+        if i >= 2 && j == 1 && i <= N-1
+             U_solut(i,j) = lam*(2* U_init(i, j+1) + U_init(i+1,j) + U_init(i-1,j)) + (1-4*lam)*(U_init(i,j)); 
+        %interior points
+        else
+             U_solut(i,j) = lam*(U_init(i,j-1)+U_init(i, j+1) + U_init(i+1,j) + U_init(i-1,j)) + (1-4*lam)*(U_init(i,j)); 
         end
     end
+
 end
+        %This finds the relative error
+        error = abs(mean(mean(U_solut)) - mean(mean(U_init)))/abs(mean(mean(U_solut)));
+    
+       if error < .001
+            disp(t)
+       break
+       end
+    surf(x,y,U_solut)
+    drawnow
 end
-mesh(x,y,U_solut)
